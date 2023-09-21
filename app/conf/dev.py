163d889c25067d928346a9b6ef485afe9ba9ee0b
Nikita <nikitabechthold@gmail.com>
2023-09-21 12:57:55 +0600

    Modified urls

--- app/conf/dev.py
+++ app/conf/dev.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 
 DEBUG = os.environ.get('DEBUG')
+PRODUCTION = os.environ.get('PRODUCTION')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
